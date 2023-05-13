fc8480940017fcca74f5c1bdd97e52c07eb2e0b0
jodoyle29 <jodoyle29@gmail.com>
2023-05-13 13:26:52 -0400

    Update sketchpad to be fully functional

--- javascript.js
+++ javascript.js
@@ -1,36 +1,22 @@
-let div = document.querySelector('.container');
 
-for(let i = 0; i < 16; ++i){
+let div = document.querySelector('.container');
+ 
+function generateCells(num){
+	width  = Math.floor(960 / num);
+	height = Math.floor(960 / num);
+
+	for(let i = 0; i < num; ++i){
 
 		let row = document.createElement('div');
 		row.className = 'row';
 
-		for(let j = 1; j <= 16; ++j){
+		for(let j = 1; j <= num; ++j){
 
 			let cell = document.createElement('div');
 			cell.className = 'cell';
-			cell.id = (i * 16) + j;
-			cell.innerText = (i * 16) + j;
-
-			row.appendChild(cell);
-		}
-
-		div.appendChild(row);
-	}
-
-/* 
-function generateCells(num){
-
-	for(let i = 0; i < num; ++i){
-
-		let row = createElement('div');
-		row.className = 'row';
-
-		for(let j = 1; j <= num; ++j){
-
-			let cell = createElement('div');
-			cell.className = 'cell';
 			cell.id = (i * num) + j;
+			cell.setAttribute('style', `width: ${width}px; 
+										height: ${height}px;`);
 
 			row.appendChild(cell);
 		}
@@ -38,4 +24,35 @@ function generateCells(num){
 		div.appendChild(row);
 	}
 } 
-*/
+
+let button = document.querySelector('button');
+
+button.addEventListener('click', function(e){
+	let gridSize = prompt("Please enter a number ( < 100)", "16");
+
+	if(gridSize != null && gridSize >= 100){
+		document.getElementById('message').innerText = 
+		"The number you have entered (" + gridSize + ") is too large";
+	}
+
+	if(gridSize != null && gridSize < 0 ){
+		document.getElementById('message').innerText = 
+		"Cannot greate a grid of negative size";
+	}
+
+	if(gridSize != null && gridSize < 100){
+		generateCells(gridSize);
+	}
+
+	[...document.querySelectorAll('.cell')].forEach(function (item){
+	item.addEventListener('mouseenter', function(e){
+		let cell = document.getElementById(e.target.getAttribute('id'));
+		cell.classList.add('newColor');
+		console.log(`Changed the background color of cell # ${e.target.getAttribute('id')}`);
+
+		});
+	});
+});
+
+
+
